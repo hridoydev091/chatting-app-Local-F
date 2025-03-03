7d6b34d3664d33bba5341a52ff99d269efb13c72
MD Hridoy Khan <mdhridoy31121997@gmail.com>
2025-03-03 23:10:59 +0600

    chatting app01

--- src/Pages/SingUP/Index.jsx
+++ src/Pages/SingUP/Index.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-
+import lib from"../../lib/lib";
 const SignUP = () => {
-  return (
+    const data = lib.signUpdata();
+    return (
     <div>
         <div className='bg-mainBgColor flex items-center justify-between'>
             <div className="w-1/2 h-screen flex justify-center items-center">
             <div>
                 <h1>Get started with easily register</h1>
                 <p>Free register and you can enjoy it</p>
-                <form action="#">
-                    <div className='flex flex-col gap-y-1 items-start'>
-                        <label htmlFor="email">email</label>
+                <form action="#" className='mt-10'>
+                    {data?.map((item)=> (
+                        <div className='flex flex-col gap-y-1 items-start'>
+                        <label htmlFor="email">{`Fail Up The ${item.name} *`}</label>
                         <input type="email"
                          placeholder='example@gmail.com'
                          name ="email"
-                         className="borde border-gray-500 py-1 px-2"
+                         className="border border-gray-500 py-1 px-2"
                          />
-                    </div>
+                    </div> 
+                    ))}
+                    
                 </form>
             </div>
             </div>
-            <div className="w-1/2 bg-yellow-500 h-svh">two</div>
+            <div className="w-1/2 bg-yellow-500 h-screen">two</div>
         </div>
     </div>
   )
---
+++ src/lib/lib.js
@@ -0,0 +1,19 @@
+const _ ={};
+_.signUpdata = ()=>{
+    const singupiterm = [
+        {
+            id: 1,
+            name:"email",
+        },
+        {
+            id: 2,
+            name:"fullname",
+        },
+        {
+            id: 3,
+            name:"password",
+        },
+];
+return singupiterm;
+};
+export default _;
